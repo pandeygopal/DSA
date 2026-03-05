fe90dfffd228be77931fdb88667b5dcfed33a86a
Gopal <145958775+pandeygopal@users.noreply.github.com>
2026-03-05 17:54:59 +0530

    Time: 39 ms (70.05%), Space: 42 MB (74.75%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.java
@@ -0,0 +1,24 @@
+class Solution {
+    int count = 0;
+
+    public int countArrangement(int n) {
+        boolean[] visited = new boolean[n + 1];
+        backtrack(n, 1, visited);
+        return count;
+    }
+
+    private void backtrack(int n, int pos, boolean[] visited) {
+        if (pos > n) {
+            count++;
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i] && (i % pos == 0 || pos % i == 0)) {
+                visited[i] = true;
+                backtrack(n, pos + 1, visited);
+                visited[i] = false;
+            }
+        }
+    }
+}
